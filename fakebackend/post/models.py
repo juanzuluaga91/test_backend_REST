f505a2ec70015b3e67689525bfb6e239f5502b9f
Juan Pablo <juanpablo@pop-os.localdomain>
2020-06-12 12:54:31 -0500

    first commit

---
+++ fakebackend/post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    file = models.FileField(upload_to='excel_files')
+    
+    def __str__(self):
+        return self.title
+
